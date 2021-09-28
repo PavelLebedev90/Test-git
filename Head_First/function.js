979f2f2915fff1b61ccb7060c2a671e21c6648b0
Pavel Lebedev <pashoktver90@gmail.com>
2021-09-28 20:22:27 +0300

    commit funktion

---
+++ Head_First/function.js
@@ -0,0 +1,16 @@
+//function bark(weight, name){
+//if(weight >20){//
+//    console.log(name + " says WOOF WOOF");
+//    } else
+//        {
+//            console.log(name + ' says woof woof');
+//        }
+//}
+//bark(19, "Ledy");
+
+function dolt(param){
+    param = 2;
+}
+let test = 1;
+dolt(test);
+//console.log(test);
