cfc21c62a3a410627e5f0d636451e2469acf5197
Pavel Lebedev <pashoktver90@gmail.com>
2021-10-06 21:18:25 +0300

    commit

---
+++ Head_First/JS_battle.js
@@ -0,0 +1,66 @@
+var view  = {
+    displayMessage: function(msg){
+        var messageArea  = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location){
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+
+    },
+    displayMiss: function(location){
+        var cell  = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};
+
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [
+        {    locations: ["10", "20", "30"],
+        hits: ["", "", ""] },
+        {    locations: ["32", "33", "34"],
+        hits: ["", "", ""] },
+        {    locations: ["63", "64", "65"],
+        hits: ["", "", ""] }
+    ],
+    fire: function(guess){
+        for(var i = 0; i < this.numShips; i++){
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if(index >=0){
+                ship.hits[index] = "hit"
+                view.displayHit(guess);
+                view.displayMessage("HIT!!");
+                if(this.isSunk(ship)){
+                    view.displayMessage("You sank my battleship!!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed!!");
+        return false;
+    },
+    isSunk: function(ship){
+        for(var i = 0; i < this.shipLength; i++){
+            if(ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+var controller = {
+    guesses: 0,
+    processGuess: function(guess){
+        
+    }
+}
